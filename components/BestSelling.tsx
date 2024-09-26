440f5165bffd455b8690956360a7a104e57cdce4
molaraiche <laraichemohamed@gmail.com>
2024-09-26 12:59:24 +0100

    updates

--- components/BestSelling.tsx
+++ components/BestSelling.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { GoArrowRight } from "react-icons/go";
 import ProductCard from "./shared/ProductCard";
 import { productType } from "@/types/types";
+import PremiumCard from "./shared/PremiumCard";
 
 const BestSelling = () => {
   return (
@@ -32,6 +33,24 @@ const BestSelling = () => {
             height={product.height}
           />
         ))}
+      </div>
+      <div className='flex gap-10 items-center justify-center my-20 lg:flex-nowrap md:flex-nowrap flex-wrap'>
+        <PremiumCard
+          title='Hight-quality Bluetooth Earbuds'
+          imgURL='/assets/earphones-hero.png'
+          height={220}
+          width={300}
+          className='rotate-[24]'
+          bg='#AF367B'
+        />
+        <PremiumCard
+          title='Headphones for all range of price'
+          imgURL='/assets/sony-headphones.png'
+          height={400}
+          width={700}
+          className='rotate-12'
+          bg='#1B234A'
+        />
       </div>
     </section>
   );
--- components/shared/PremiumCard.tsx
+++ components/shared/PremiumCard.tsx
@@ -1,7 +1,43 @@
-import React from "react";
-
-const PremiumCard = () => {
-  return <div>PremiumCard</div>;
+import Image from "next/image";
+interface premiumCardTypee {
+  title: string;
+  imgURL: string;
+  width: number;
+  height: number;
+  className?: string;
+  bg: string;
+}
+const PremiumCard = ({
+  title,
+  imgURL,
+  width,
+  height,
+  className,
+  bg,
+}: premiumCardTypee) => {
+  return (
+    <div
+      style={{
+        backgroundColor: bg,
+      }}
+      className='flex lg:flex-row flex-col items-center justify-between w-[700px] lg:h-[400px] h-[600px] py-14 px-10  rounded-tr-[10px] rounded-tl-[90px] rounded-bl-[10px] rounded-br-[90px] text-white'>
+      <div className=''>
+        <h1 className='text-[27px] font-medium leading-[43px]'>{title}</h1>
+        <button className=' mt-3 border-2 rounded-[10px] border-primary py-2.5 px-8 bg-dark-white text-secondary'>
+          Buy Now
+        </button>
+      </div>
+      <div className=''>
+        <Image
+          src={imgURL}
+          alt={title}
+          width={width}
+          height={height}
+          className={className}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PremiumCard;
