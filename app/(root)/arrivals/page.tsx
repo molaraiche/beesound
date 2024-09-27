5554cc88cdc19e72ba430d0f12004bb0618eecba
molaraiche <laraichemohamed@gmail.com>
2024-09-27 04:45:29 +0100

    set the products pages

--- app/(root)/arrivals/page.tsx
+++ app/(root)/arrivals/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import ProductCard from "@/components/shared/ProductCard";
+import { products } from "@/constants/products";
+import { productType } from "@/types/types";
 
 const Arrivals = () => {
-  return <div>Arrivals</div>;
+  return (
+    <section className='lg:container lg:mx-auto md:px-14 sm:px-10 xsm:px-4 flex flex-wrap items-center justify-center my-10'>
+      <div className='flex gap-8 justify-center items-center mt-20 flex-wrap'>
+        {products.newArrivals.map((product: productType) => (
+          <ProductCard
+            key={product.id}
+            price={product.price}
+            title={product.title}
+            imgURL={product.image}
+            colors={product.colors}
+            width={product.width}
+            height={product.height}
+            classeName='hover:scale-110	transition-all'
+          />
+        ))}
+      </div>
+      <div className='flex gap-8 justify-center items-center mt-20 flex-wrap'>
+        {products.newArrivals.map((product: productType) => (
+          <ProductCard
+            key={product.id}
+            price={product.price}
+            title={product.title}
+            imgURL={product.image}
+            colors={product.colors}
+            width={product.width}
+            height={product.height}
+            classeName='hover:scale-110	transition-all'
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Arrivals;
--- app/(root)/gamers/page.tsx
+++ app/(root)/gamers/page.tsx
@@ -1,7 +1,41 @@
+import ProductCard from "@/components/shared/ProductCard";
+import { products } from "@/constants/products";
+import { productType } from "@/types/types";
 import React from "react";
 
 const Gamers = () => {
-  return <div>Gamers</div>;
+  return (
+    <section className='lg:container lg:mx-auto md:px-14 sm:px-10 xsm:px-4 flex flex-wrap items-center justify-center my-10'>
+      <div className='flex gap-8 justify-center items-center flex-wrap'>
+        {products.gamers.map((product: productType) => (
+          <ProductCard
+            key={product.id}
+            price={product.price}
+            title={product.title}
+            imgURL={product.image}
+            colors={product.colors}
+            width={product.width}
+            height={product.height}
+            classeName='rotate-[20deg] hover:scale-110	transition-all'
+          />
+        ))}
+      </div>
+      <div className='flex gap-8 justify-center items-center mt-20 flex-wrap'>
+        {products.gamers.map((product: productType) => (
+          <ProductCard
+            key={product.id}
+            price={product.price}
+            title={product.title}
+            imgURL={product.image}
+            colors={product.colors}
+            width={product.width}
+            height={product.height}
+            classeName='hover:scale-110	transition-all'
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Gamers;
